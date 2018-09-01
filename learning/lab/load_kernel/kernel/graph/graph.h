556f2db8de8efc448797a33e42457870215d0d9b
Kane <v_qingxwang@tencent.com>
2018-09-01 22:00:08 +0800

    打印字符

--- learning/lab/load_kernel/kernel/graph/graph.h
+++ learning/lab/load_kernel/kernel/graph/graph.h
@@ -2,4 +2,8 @@ void print_pixel(unsigned int row, int column, int color);
 
 void print_line(unsigned int row, unsigned int color);
 
-void print_letter_b();
+void print_8x8(char map[], unsigned int row_start, int column_start, int color);
+
+void print_char(char letter, unsigned int row, int column, int color);
+
+void print_ascii(int ascii_index, unsigned int row, int column, int color);
--- learning/lab/load_kernel/kernel/main.c
+++ learning/lab/load_kernel/kernel/main.c
@@ -1,18 +1,14 @@
 #include "graph/graph.h"
 
+void Start_Kernel(void)
+{
 
-void Start_Kernel(void){
-   
     print_line(300, 0xff0000);
 
-    print_letter_b();
+    print_char('c', 0x10, 40, 0x00cc66);
+    print_char('f', 0x20, 40, 0x666666);
+    print_char('w', 0x30, 40, 0x666666);
 
-
-
-    while(1)
-    ;
-
+    while (1)
+        ;
 }
-
-
-
